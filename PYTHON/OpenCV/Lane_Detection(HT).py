403ea11277d07f73edd5b6f6e2f0ef2c149480ae
Aaditya1149 <legitbeast1149@gmail.com>
2023-12-10 12:34:30 +0530

    added lane detection code (OpenCV / Deep Learning)

--- PYTHON/OpenCV/Lane_Detection(HT).py
+++ PYTHON/OpenCV/Lane_Detection(HT).py
@@ -1,12 +1,12 @@
 import cv2 as cv
 import numpy as np
 
-capture = cv.VideoCapture('C:/Users/hp/Downloads/videoplayback.mp4', )
+capture = cv.VideoCapture('C:/Users/hp/Downloads/drive.mp4', )
 
 while True:
     ed, frame = capture.read()
     if not ed:
-        capture = cv.VideoCapture('C:/Users/hp/Downloads/videoplayback.mp4', )
+        capture = cv.VideoCapture('C:/Users/hp/Downloads/drive.mp4', )
         continue
 
     # Blur
@@ -22,17 +22,17 @@ while True:
     mask = cv.inRange(hsv, low_color, upper_color)
 
     # Edge Detection
-    canny = cv.Canny(mask, 340, 400)
+    canny = cv.Canny(mask, 100, 120)
 
     # Houghlines
-    lines = cv.HoughLinesP(canny, rho = 1, theta = 1*np.pi/100, threshold = 20, minLineLength = 40, maxLineGap = 20)
+    lines = cv.HoughLinesP(canny, rho = 2, theta = 1*np.pi/180, threshold = 50, minLineLength = 10, maxLineGap = 80)
     if lines is not None:
         for i in lines:
             x1, x2, y1, y2 = i[0]
             cv.line(frame, (x1, x2), (y1, y2), (0,255,0), 3)
     # Showing Image
     cv.imshow('Video', frame)
-    cv.imshow('Mask', canny)
+    # cv.imshow('Mask', canny)
 
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
