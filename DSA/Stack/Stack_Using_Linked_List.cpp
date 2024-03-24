8fe2e3ba866736263a4ecb98d0ae93c936ccf0ff
Aaditya1149 <legitbeast1149@gmail.com>
2024-03-24 15:22:40 +0530

    Stack_Using_Linked_List

--- DSA/Stack/Stack_Using_Linked_List.cpp
+++ DSA/Stack/Stack_Using_Linked_List.cpp
@@ -1,1 +1,31 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+struct node {
+    struct node* next;
+    int val;
+};
+
+void printList(struct node* head){
+    struct node* ptr = head;
+    while(ptr != NULL){
+        cout << ptr->val << " ";
+        ptr = ptr->next;
+    }
+}
+
+struct node* insert(struct node* head, int data){
+    struct node* newnode = new node;
+    newnode->val = data;
+    newnode->next = head;
+    return newnode;
+}
+
+int main(){
+    int n = 5;
+    struct node* head = NULL;
+    while(n--){
+        head = insert(head, n+1);
+    }
+    printList(head);
+}
